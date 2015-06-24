e452b40707aa5cced4741741531273441dc4c524
Alejandro Reyes <alejandroereyes@gmail.com>
2015-06-24 18:10:56 -0500

    added react nav bar component

---
+++ app/assets/javascripts/components/navigation.js.jsx
@@ -0,0 +1,24 @@
+'use strict';
+
+var Navigation = React.createClass({
+  render: function () {
+    return (
+      <nav>
+        <NavLink name='Home'      url='/' />
+        <NavLink name='Companies' url='/companies' />
+        <NavLink name='Courses'   url='/courses' />
+        <NavLink name='Employees' url='/employees' />
+      </nav>
+      );
+  }
+});
+
+var NavLink = React.createClass({
+  render: function () {
+    return (<a onClick={this.clicked} className='btn btn-xs'>{this.props.name}</a>);
+  },
+
+  clicked: function () {
+    window.location.href = this.props.url;
+  }
+});
